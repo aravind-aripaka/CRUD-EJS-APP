5159421f25d2563c541d5e9635972712889232a3
A3HAX <69230699+A3HAX@users.noreply.github.com>
2022-05-30 18:54:45 +0530

    last

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,7 @@ $("#update_user").submit(function(event){
     console.log(data);
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `https://nyxwolf-intern-aravind.herokuapp.com/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -29,7 +29,7 @@ if(window.location.pathname=="/"){
     $ondelete.click(function(){
         var id = $(this).attr("data-id");
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `https://nyxwolf-intern-aravind.herokuapp.com/api/users/${id}`,
             "method" : "DELETE",
         }
 
